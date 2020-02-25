d0c8091122ba3f73cd131df5bf88e6836f7e20f4
Kyle Zhao <kzhao.sw@gmail.com>
2020-02-25 18:58:10 -0500

    add a simple setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+DOT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+ln -sfn "${DOT_PATH}/zsh/zshrc" ~/.zshrc
+
+ln -sfn "${DOT_PATH}/tmux/tmux.conf" ~/.tmux.conf
+
+rm -rf ~/.vim
+ln -sfn "${DOT_PATH}/vim" ~/.vim
+
+mkdir -p ~/.config
+
+rm -rf ~/.config/git
+ln -sfn "${DOT_PATH}/git" ~/.config/git
+
+rm -rf ~/.config/nvim
+ln -sfn "${DOT_PATH}/nvim" ~/.config/nvim
+
+rm -rf ~/.config/kitty
+ln -sfn "${DOT_PATH}/kitty" ~/.config/kitty
