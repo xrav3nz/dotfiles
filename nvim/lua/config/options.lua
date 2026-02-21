fc7d97da19155245498818ef267d8798bd89b70a
Kyle <github@kylezh.com>
2026-02-21 15:29:25 -0500

    vim: TIL `breakindent`
    I hated how line wrapping looks... until I found out about `breakindent`
    
    Also played with `breakindentopt=shift:2` for a while but not sure if i like
    it yet. Enjoying `showbreak` more than I expected, though.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -16,6 +16,12 @@ vim.g.lazyvim_ruby_formatter = "standardrb"
 vim.g.lazyvim_prettier_needs_config = true
 vim.g.lazyvim_eslint_auto_format = true
 
+-- Soft wrap at word boundaries with a visual indent
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.showbreak = "↪ "
+vim.opt.breakindent = true
+
 -- Revert some LazyVim options
 
 -- I am used to scrolloff=0 so that H is the first line of the window.
