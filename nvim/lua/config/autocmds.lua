a40982f39dfb751b0e88a17519c8082f63cc4eac
Kyle <github@kylezh.com>
2026-02-22 17:02:58 -0500

    nvim/tmux: improve active pane/split visibility

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Only show cursorline in the active window
+local cursorline_group = vim.api.nvim_create_augroup("active_cursorline", { clear = true })
+vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "FocusGained" }, {
+  group = cursorline_group,
+  callback = function()
+    vim.wo.cursorline = true
+  end,
+})
+vim.api.nvim_create_autocmd({ "WinLeave", "FocusLost" }, {
+  group = cursorline_group,
+  callback = function()
+    vim.wo.cursorline = false
+  end,
+})
